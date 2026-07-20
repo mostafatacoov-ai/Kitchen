b1fff531b6d4f7263c838fe00534ac84216dc61f
mostafatacoov <mostafatacoov@gmail.com>
2026-07-20 14:23:21 +0300

    fix: db connection breaking next build

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
+// Check is moved inside dbConnect to avoid breaking Next.js build step
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
@@ -20,6 +16,12 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!process.env.MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
@@ -29,7 +31,7 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
